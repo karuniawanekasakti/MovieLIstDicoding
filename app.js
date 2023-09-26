349a4d1b53c32549a4fbe900b8316b70c72a65e4
karuniawanekasakti <karuniawankaka80@mail.ugm.ac.id>
2023-09-26 22:27:15 +0700

    add webpack environment

--- app.js
+++
@@ -1,7 +0,0 @@
-import Main from "./src/script/data/main.js";
-import "./src/script/component/appbar.js";
-import "./src/script/component/paginationComponent.js";
-
-const mainInit = new Main();
-
-document.addEventListener("DOMContentLoaded", mainInit);
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Main from "./script/data/main.js";
+import "./script/component/appbar.js";
+import "./script/component/paginationComponent.js";
+import "./styles/style.css";
+
+const mainInit = new Main();
+
+document.addEventListener("DOMContentLoaded", mainInit);
